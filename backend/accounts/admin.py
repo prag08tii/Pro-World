79118e01ffc561ab626419c9c83018bfef49c553
prag08tii <gondkarpragati@gmail.com>
2026-04-02 00:37:05 +0530

    all updated

---
+++ backend/accounts/admin.py
@@ -0,0 +1,9 @@
+# Register your models here.
+from django.contrib import admin
+from .models import User, Domain, Application, Student, Mentor
+
+admin.site.register(User)
+admin.site.register(Domain)
+admin.site.register(Application)
+admin.site.register(Student)
+admin.site.register(Mentor)
